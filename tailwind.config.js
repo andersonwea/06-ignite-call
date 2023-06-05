99381a43d1455c9e9b2a34f6e24cb5ca17dad5f3
chain <klaus.mika1052@gmail.com>
2023-06-05 15:38:49 -0300

    feat: add tailwind variables and blobal styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,28 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        white: '#FFF',
+        black: '#000',
+
+        'gray-100': '#E1E1E6',
+        'gray-200': '#A9A9B2',
+        'gray-400': '#7C7C8A',
+        'gray-500': '#505059',
+        'gray-600': '#323238',
+        'gray-700': '#29292E',
+        'gray-800': '#202024',
+        'gray-900': '#121214',
+
+        'ignite-300': '#00B37E',
+        'ignite-500': '#00875F',
+        'ignite-700': '#015F43',
+        'ignite-900': '#00291D',
+      },
+
+      fontFamily: {
+        default: 'var(--font-roboto)',
+        code: 'monospace',
       },
     },
   },
