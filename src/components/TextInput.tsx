b6f166a4aa7b2dca53caa858d35a18b94fa27c24
chain <klaus.mika1052@gmail.com>
2023-06-05 17:29:24 -0300

    feat: add text input component

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,37 @@
+import { InputHTMLAttributes } from 'react'
+import clsx from 'clsx'
+
+interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  prefix?: string
+}
+
+export function TextInput({ prefix, ...props }: TextInputProps) {
+  return (
+    <div
+      className={clsx(
+        'rounded-md border-2 border-solid border-gray-900 bg-gray-900 px-4 py-3',
+        'flex items-center',
+        'focus:border-ignite-300',
+        'disabled:cursor-not-allowed disabled:opacity-50',
+        'focus-within:border-ignite-300',
+      )}
+    >
+      {!!prefix && (
+        <span className="font-default text-sm font-normal text-gray-400">
+          {prefix}
+        </span>
+      )}
+      <input
+        className={clsx(
+          'b-0 w-full bg-transparent font-default text-sm font-normal text-white',
+          'focus:outline-none',
+          'disabled:cursor-not-allowed',
+          'placeholder:text-gray-400',
+        )}
+        {...props}
+      />
+    </div>
+  )
+}
+
+TextInput.displayName = 'TextInput'
