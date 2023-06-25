5050a511250f46a23e773b35ee3b45899cf28c56
chain <klaus.mika1052@gmail.com>
2023-06-25 00:02:51 -0300

    feat: config react-query

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,12 +3,26 @@
 import { SessionProvider } from 'next-auth/react'
 import { ReactNode } from 'react'
 import { Session } from 'next-auth'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/react-query'
 
 interface GoogleProviderProps {
   children: ReactNode
   session: Session | null
 }
 
+interface QueryProviderProps {
+  children: ReactNode
+}
+
 export function GoogleProvider({ children, session }: GoogleProviderProps) {
   return <SessionProvider session={session}>{children}</SessionProvider>
+}
+
+export function QueryProvider({ children }: QueryProviderProps) {
+  // const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
 }
